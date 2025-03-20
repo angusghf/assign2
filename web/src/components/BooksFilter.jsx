288d47950bb1d30f8d429b8e42168717c00c34dc
angusghf <159850524+angusghf@users.noreply.github.com>
2025-03-20 16:09:39 -0700

    filter start

--- web/src/components/BooksFilter.jsx
+++ web/src/components/BooksFilter.jsx
@@ -1,59 +1,34 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
+
 import BFC from '../components/BookFilters.module.css';
 
-const BooksFilter = () => {
-    const [selectedFilters, setSelectedFilters] = useState([]);
+function BookFilters() {
 
-    const handleFilterChange = (event) => {
-        const { value, checked } = event.target;
-        if (checked) {
-            setSelectedFilters((prevFilters) => [...prevFilters, value]);
-        } else {
-            setSelectedFilters((prevFilters) =>
-                prevFilters.filter((filter) => filter !== value)
-            );
-        }
-    };
+    const [authors, setAuthors] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/authors")
+            .then((response) => response.json())
+            .then( data => {
+                setAuthors(data);
+                console.log("---authors---");
+                console.log(data);
+                setAuthors(data);
+            });
+    }, []);
 
     return (
-<div className={BFC['filters-container']}>
-            <h2>Books Filter</h2>
-            <label>
-                <input
-                    type="checkbox"
-                    value="Fiction"
-                    onChange={handleFilterChange}
-                />
-                Jeff Lindsay
-            </label>
-            <label>
-                <input
-                    type="checkbox"
-                    value="Fiction"
-                    onChange={handleFilterChange}
-                />
-                JD Salinger
-            </label>
-            <label>
-                <input
-                    type="checkbox"
-                    value="Fiction"
-                    onChange={handleFilterChange}
-                />
-                Kenneth Oppel
-            </label>
-            <label>
-                <input
-                    type="checkbox"
-                    value="Fiction"
-                    onChange={handleFilterChange}
-                />
-                Ryan Holiday
-            </label>
-            
-            {/* Add more checkbox labels as needed */}
+        <div className={BFC['filters-container']}>
+            <form>
+                <h4>Authors</h4>
+                <label>
+                    <input type="checkbox" name="author" />
+                    Author Name
+                </label>
+                <input type="submit" value="Apply" />
+            </form>
         </div>
     );
-};
+}
 
-export default BooksFilter;
+export default BookFilters;
