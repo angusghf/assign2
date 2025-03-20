3672c1700f73447f90941f65056ebc3d974af58e
angusghf <159850524+angusghf@users.noreply.github.com>
2025-03-20 14:26:37 -0700

    functionality to add stuff to db

--- api/routers/books.js
+++ api/routers/books.js
@@ -23,10 +23,24 @@ booksRouter.get("/", (req, res) => {
 
 booksRouter.post("/", upload.single("image"), (req, res) => {
     const { author_id, title } = req.body;
-    const imageName = req.file.filename;
-    console.log("author:", author_id);
-    console.log("title:", title);
-    console.log("imageName:", imageName);
+    const image_name = req.file.filename;
+
+    // console.log("author:", author_id);
+    // console.log("title:", title);
+    // console.log("imageName:", imageName);
+
+    const addNovelSQL = `INSERT INTO novels (author_id, name, image_name) VALUES (?,?,?)`;
+    db.query(addNovelSQL, [author_id, title, image_name], (err, results) => {
+
+        if (err) {
+            console.log(err);
+            return res.status(500).send("An error has occured!");
+        }
+
+        res.status(200).json({message: "Book added successfully!"})
+
+    })
+
 });
 
 module.exports = booksRouter;
