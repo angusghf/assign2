7e5f3cccc62c986b7f8eb800d8246d1d947831aa
angusghf <159850524+angusghf@users.noreply.github.com>
2025-03-19 21:36:49 -0700

    form

--- web/src/components/AddBookModalContent.jsx
+++ web/src/components/AddBookModalContent.jsx
@@ -13,7 +13,40 @@ function ModalContent({ onClose }) {
     return (
         <div className={m["modal-container"]}>
             <div className={`${m["modal"]} ${ABM["book-card"]}`}>
-                <h4>I'm a modal</h4>
+                <h3>Add a Book</h3>
+                <form className={ABM["form-group"]} encType="multipart/form-data">
+                    {/* AUTHOR NAME */}
+                    <label htmlFor="bookAuthor">Author:</label>
+                    <select
+                        name="author"
+                        id="author"
+                        value={author}
+                    >
+                        {dbAuthors && dbAuthors.map((author, index) => (
+                            <option key={author.id} value={author.id}>{author.name}</option>
+                        ))}
+                    </select>
+
+                    {/* TITLE OF BOOK */}
+                    <label htmlFor="bookTitle">Title:</label>
+                    <input
+                        type="text"
+                        name="title"
+                        id="bookTitle"
+                        onChange={(event) => { setTitle(event.target.value) }}
+                    />
+
+                    {/* IMAGE */}
+                    <label htmlFor="image">Image</label>
+                    <input type="file"
+                        name="image"
+                        id="image"
+                        onChange={(e) => setImage(e.target.files[0])} />
+
+                    <div className={ABM['spacer']}>
+                        <button className={ABM['add-button']} type="submit">Add tape</button>
+                    </div>
+                </form>
                 <button className={m["close-button"]} onClick={onClose}>Close</button>
             </div>
         </div>
